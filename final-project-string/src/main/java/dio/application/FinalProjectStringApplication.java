7ce5a68f50ba0b5417ef53b92679bc5c7e18f72a
Robert <robertgleison12@gmail.com>
2023-09-11 14:17:10 +0100

    Created initial Spring project

---
+++ final-project-string/src/main/java/dio/application/FinalProjectStringApplication.java
@@ -0,0 +1,13 @@
+package dio.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectStringApplication.class, args);
+	}
+
+}
---
+++ final-project-string/src/test/java/dio/application/FinalProjectStringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
