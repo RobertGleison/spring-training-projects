c8a7738d1ced465ac10d24e3a7759c64986c3b33
Robert <robertgleison12@gmail.com>
2023-09-22 10:15:15 +0100

    Implemented Order

---
+++ firstSpringProject/src/main/java/services/OrderService.java
@@ -0,0 +1,29 @@
+package services;
+
+import entities.Order;
+import entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.OrderRepository;
+import repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj =  repository.findById(id);
+        return obj.get();
+    }
+
+
+}
