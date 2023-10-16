08c8cd4c6da6c55edfba33db7e63e3e1c135f11e
Robert <robertgleison12@gmail.com>
2023-10-16 23:28:51 +0100

    Created Flyway migrations

---
+++ basic-crud/src/main/resources/db/migration/V2__Inserting_Products.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (Id, Name, Description, Price)
+VALUES
+(1,'TV','Full HD TV', 1000.50),
+(2,'Computer','Intel i7', 2100.00),
+(3,'Laptop','Acer Nitro 5', 900.00),
+(4,'Mouse','Redragon Cobra', 50.00),
+(5,'Cellphone','galaxy Note 10 lite', 850.00);
