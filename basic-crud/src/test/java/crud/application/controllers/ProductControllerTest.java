b6e1d985c9925669a3659d07e5df063c1b47655f
Robert <robertgleison12@gmail.com>
2023-10-17 17:37:38 +0100

    Created ProductControllerTest file for test with JUnit5 and Mockito

---
+++ basic-crud/src/test/java/crud/application/controllers/ProductControllerTest.java
@@ -0,0 +1,28 @@
+package crud.application.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void update() {
+    }
+}
