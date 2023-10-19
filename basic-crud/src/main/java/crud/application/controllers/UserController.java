f3e153e4ac68dc6af1923213360ed02b3a1e4d8f
Robert <robertgleison12@gmail.com>
2023-10-19 15:55:51 +0100

    Implemented a few methods of UserController

--- basic-crud/src/main/java/crud/application/controllers/UserController.java
+++ basic-crud/src/main/java/crud/application/controllers/UserController.java
@@ -1,12 +1,39 @@
 package crud.application.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import crud.application.entities.Product;
+import crud.application.entities.User;
+import crud.application.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService service;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Integer id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+
+    @PostMapping
 
 
 
--- basic-crud/src/main/java/crud/application/services/UserService.java
+++ basic-crud/src/main/java/crud/application/services/UserService.java
@@ -3,6 +3,7 @@ package crud.application.services;
 import crud.application.entities.User;
 import crud.application.repositories.UserRepository;
 import crud.application.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,24 +15,41 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
 
-    public User findById(Integer id){
+    public User findById(Integer id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
-    public void deleteById(Integer id){
+    public void deleteById(Integer id) {
         Optional<User> user = repository.findById(id);
-        if(user == null) throw new ResourceNotFoundException(id);
+        if (user == null) throw new ResourceNotFoundException(id);
         else repository.deleteById(id);
     }
 
+    public User insert(User user) {
+        return repository.save(user);
+    }
 
+    public User update(User user, Integer id) {
+        try {
+            User oldUser = repository.getReferenceById(id);
+            updateData(oldUser, user);
+            return repository.save(oldUser);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
 
-
+    private void updateData(User oldUser, User newUser) {
+        newUser.setName(oldUser.getName());
+        newUser.setEmail(oldUser.getEmail());
+        newUser.setPassword(oldUser.getPassword());
+        newUser.setPhone(oldUser.getPhone());
+    }
 
 
 }
