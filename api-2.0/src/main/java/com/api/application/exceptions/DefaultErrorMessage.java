edbf81cbe274360523809cef405465316ebbb338
Robert <robertgleison12@gmail.com>
2023-11-30 13:01:56 +0000

    Implemented exception handling

---
+++ api-2.0/src/main/java/com/api/application/exceptions/DefaultErrorMessage.java
@@ -0,0 +1,52 @@
+package com.api.application.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.Instant;
+import java.time.LocalTime;
+
+public class DefaultErrorMessage {
+    private String message;
+    private String error;
+    private LocalTime timestamp;
+    private Integer status;
+
+    public DefaultErrorMessage(String message, String error, LocalTime timestamp, Integer status) {
+        this.message = message;
+        this.error = error;
+        this.timestamp = timestamp;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public LocalTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
---
+++ api-2.0/src/main/java/com/api/application/exceptions/ExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.api.application.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.time.LocalTime;
+
+@ControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<DefaultErrorMessage> handleResourceNotFoundException(ResourceNotFoundException exception){
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        DefaultErrorMessage response = new DefaultErrorMessage(exception.getMessage(), error, LocalTime.now(),status.value());
+        return ResponseEntity.status(status).body(response);
+    }
+}
